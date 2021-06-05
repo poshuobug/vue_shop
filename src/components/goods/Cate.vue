dfe58ac88fc7ce868f6e39c52d3928266d5d7ee0
zgz <1457131838@qq.com>
2021-06-05 13:42:26 +0800

    完成商品分类功能的开发

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -20,15 +20,13 @@
         index-text="#"
         border
         :show-row-hover="false"
-        class="treeTable"
-      >
+        class="treeTable">
         <!-- 是否有效 -->
         <template slot="isok" slot-scope="scope">
           <i
             class="el-icon-success"
             v-if="scope.row.cat_deleted === false"
-            style="color: lightgreen"
-          ></i>
+            style="color: lightgreen"></i>
           <i class="el-icon-error" v-else style="color: red"></i>
         </template>
         <!-- 排序 -->
@@ -37,9 +35,7 @@
           <el-tag
             v-else-if="scope.row.cat_level === 1"
             type="success"
-            size="mini"
-            >二级</el-tag
-          >
+            size="mini">二级</el-tag>
           <el-tag v-else type="warning" size="mini">三级</el-tag>
         </template>
         <!-- 操作 -->
@@ -56,39 +52,42 @@
         :page-sizes="[1, 3, 5, 8]"
         :page-size="queryInfo.pagesize"
         layout="total, sizes, prev, pager, next, jumper"
-        :total="total"
-      >
+        :total="total">
       </el-pagination>
     </el-card>
     <!-- 添加分类的对话框 -->
     <el-dialog
-  title="添加分类"
-  :visible.sync="addCateDialogVisible"
-  width="50%"
-  @close="addCateDialogClosed">
-  <!-- 表单 -->
-  <el-form :model="addCateForm" :rules="addCateFormRules" ref="addCateFormRef" label-width="100px">
-  <el-form-item label="分类名称：" prop="cat_name">
-    <el-input v-model="addCateForm.cat_name"></el-input>
-  </el-form-item>
-  <el-form-item label="父级分类：" prop="cat_level">
-    <!-- 级联 -->
-    <el-cascader
-    v-model="selectedKeys"
-    :options="ParentCateList"
-    change-on-select
-    expand-trigger="hover"
-    :props="cascaderProps"
-    @change="parentCateChanged"
-    clearable>
-    </el-cascader>
-  </el-form-item>
-  </el-form>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="addCateDialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="addCate">确 定</el-button>
-  </span>
-</el-dialog>
+      title="添加分类"
+      :visible.sync="addCateDialogVisible"
+      width="50%"
+      @close="addCateDialogClosed">
+      <!-- 表单 -->
+      <el-form
+        :model="addCateForm"
+        :rules="addCateFormRules"
+        ref="addCateFormRef"
+        label-width="100px">
+        <el-form-item label="分类名称：" prop="cat_name">
+          <el-input v-model="addCateForm.cat_name"></el-input>
+        </el-form-item>
+        <el-form-item label="父级分类：" prop="cat_level">
+          <!-- 级联 -->
+          <el-cascader
+            v-model="selectedKeys"
+            :options="ParentCateList"
+            change-on-select
+            expand-trigger="hover"
+            :props="cascaderProps"
+            @change="parentCateChanged"
+            clearable>
+          </el-cascader>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addCateDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 <script>
@@ -191,9 +190,11 @@ export default {
     },
     // 获取父级分类的数据列表
     async getParentCateList() {
-      const { data: res } = await this.$http.get('categories', { params: {
-        type: 2
-      }})
+      const { data: res } = await this.$http.get('categories', {
+        params: {
+          type: 2
+        }
+      })
       if (res.meta.status !== 200) return this.$message.error('获取分类数据列表失败！')
       this.ParentCateList = res.data
     },
@@ -202,7 +203,8 @@ export default {
       // 大于0 选中父级分类
       if (this.selectedKeys.length > 0) {
         // 父级分类的id
-        this.addCateForm.cat_pid = this.selectedKeys[this.selectedKeys.length - 1]
+        this.addCateForm.cat_pid =
+          this.selectedKeys[this.selectedKeys.length - 1]
         // 分类的等级
         this.addCateForm.cat_level = this.selectedKeys.length
       } else {
@@ -218,9 +220,12 @@ export default {
       this.selectedKeys = []
     },
     addCate() {
-      this.$refs.addCateFormRef.validate(async vaild => {
+      this.$refs.addCateFormRef.validate(async (vaild) => {
         if (!vaild) return
-        const { data: res } = await this.$http.post('categories', this.addCateForm)
+        const { data: res } = await this.$http.post(
+          'categories',
+          this.addCateForm
+        )
         if (res.meta.status !== 201) return this.$message.error('添加失败！')
       })
       this.$message.success('添加成功！')
